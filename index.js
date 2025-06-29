cb715e593a3e891788d403f830edc09726d88fb1
Abdul Rehman <abdulrehman@192.168.1.5>
2025-06-29 18:10:48 +0500

    multiply function

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function sum(a, b){
 console.log("Result of sum function: ", sum(2,3));
 result = sum(5,3);
 console.log("The sum is: " + result);
+
+
+function multiply(a,b){
+    return a * b;
+}
+
+console.log("Result of multiply function: ", multiply(2,3));
+result = multiply(5,3);
+console.log("The product is: " + result);
